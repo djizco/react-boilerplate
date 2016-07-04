5df7ad2b135d25e365d96803dd6c9444c0b10daf
Rico Rojas <djizco@yahoo.com>
2016-07-04 05:44:25 -0500

    Add CSS to webpack

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React    from 'react';
 import {render} from 'react-dom';
 import routes   from './config/routes.js';
+import styles   from './main.css';
 
 render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/, loader: "style-loader!css-loader"
       }
     ]
   },
