39c9b3ac60c00f4d95204578bda3edcd151393a5
Rico Rojas <djizco@yahoo.com>
2016-07-10 04:56:41 -0500

    Use browserHistory only with a real server

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import Root           from './containers/Root.js';
@@ -10,8 +9,15 @@ import styles         from './styles/main.css';
 import scss           from './styles/main.scss';
 import configureStore from './store/configureStore';
 
+let historyType;
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development'){
+  historyType = require('react-router').browserHistory;
+} else {
+  historyType = require('react-router').hashHistory;
+}
+
 let store = configureStore();
-let history = syncHistoryWithStore(browserHistory, store);
+let history = syncHistoryWithStore(historyType, store);
 
 render(
   <Root store={store} history={history} />,
