9db7ef4f69bf3a3988416c5ef2af001f4bb2bb59
Rico Rojas <djizco@yahoo.com>
2016-08-20 20:13:41 -0500

    Lint script, update package.json, add Code/Lab

--- test/bootstrap.js
+++
@@ -1,7 +0,0 @@
-//
-// This code is to allow all test files to easily require
-// the test helper file, regardless of nested folder location.
-//
-// e.g. require(TEST_HELPER)
-//
-global.TEST_HELPER = __dirname + '/test-helper.js';
