539cc3c413eacae953be4ba7a05ddb719a0a877f
Rico Rojas <djizco@yahoo.com>
2016-08-23 01:02:33 -0500

    Center items in Todo App

--- client/components/AddTodo.js
+++ client/components/AddTodo.js
@@ -12,9 +12,13 @@ export default class AddTodo extends Component {
 
   render() {
     return (
-      <div className="add-todo">
-        <input type="text" ref={c => { this.input = c; }} />
-        <button className="btn btn-sm btn-border btn-success" onClick={this.handleClick}>Add</button>
+      <div className="add-todo col-sm-12">
+        <input className="col-sm-10" type="text" ref={c => { this.input = c; }} />
+        <button
+          className="btn btn-sm btn-border btn-success col-sm-1 col-sm-offset-1"
+          onClick={this.handleClick} >
+          Add
+        </button>
       </div>
     );
   }
