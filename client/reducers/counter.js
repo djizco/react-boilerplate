5a029a222c0d13f2dd59771a4f649f58bb64cd61
Rico Rojas <djizco@yahoo.com>
2016-08-22 21:08:08 -0500

    Fix import route

--- client/reducers/counter.js
+++ client/reducers/counter.js
@@ -1,4 +1,4 @@
-import { INCREMENT, DECREMENT } from '../actions/index';
+import { INCREMENT, DECREMENT } from '../actions/counter';
 
 export default function counter(state = 0, action) {
   switch (action.type) {
