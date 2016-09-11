a71cac6ffa5d9a98ca8858fc76726c3aff3c3cbf
Rico Rojas <djizco@yahoo.com>
2016-09-11 12:54:18 -0500

    Fix React CSS Transitions

--- client/containers/Main.js
+++ client/containers/Main.js
@@ -1,19 +1,17 @@
 import React, { PropTypes } from 'react';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import TransitionGroup from 'react-addons-css-transition-group';
 import Navigation from './Navigation';
 
 export default function Main(props) {
   return (
     <div className="main-container">
       <Navigation />
-      {/* <ReactCSSTransitionGroup
-        component="div"
+      <TransitionGroup
         transitionName="appear"
         transitionEnterTimeout={500}
-        transitionLeaveTimeout={500}>
+        transitionLeaveTimeout={300}>
         {React.cloneElement(props.children, { key: props.location.pathname })}
-      </ReactCSSTransitionGroup> */}
-      {props.children}
+      </TransitionGroup>
     </div>
   );
 }
