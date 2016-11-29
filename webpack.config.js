ac465811ce2351ff4ae65c6a53fdd5f43aa59e0d
Rico Rojas <djizco@yahoo.com>
2016-11-29 00:38:32 -0600

    Fix webpack config bug.

--- webpack.config.js
+++ webpack.config.js
@@ -16,49 +16,50 @@ module.exports = {
     contentBase: './dist',
     colors: true,
     historyApiFallback: true,
-    inline: true
+    inline: true,
   },
   entry: [
-    './client/index.js'
+    './client/index.js',
   ],
   output: {
+    filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    publicPath: '/',
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css')
+        loader: ExtractTextPlugin.extract('style', 'css'),
       },
       {
         test: /\.scss$/i,
-        loader: ExtractTextPlugin.extract(['css', 'sass'])
+        loader: ExtractTextPlugin.extract(['css', 'sass']),
       },
       {
         test: /\.less$/i,
-        loader: ExtractTextPlugin.extract(['css', 'less'])
+        loader: ExtractTextPlugin.extract(['css', 'less']),
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
         loaders: [
           'file?name=[name].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        ],
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      }
+        loader: 'file-loader',
+      },
     ]
   },
   plugins: [
