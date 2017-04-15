c56c06e556abc846e30253932fe35dca17e3f71a
Rico Rojas <djizco@yahoo.com>
2017-04-15 16:11:20 -0500

    Update reducer

--- client/reducers/todos.js
+++ client/reducers/todos.js
@@ -2,7 +2,7 @@ import update from 'immutability-helper';
 import R from 'ramda';
 import { ADD_TODO, TOGGLE_COMPLETE_TODO, UPDATE_TODO, DELETE_TODO } from '../actions/todos';
 
-export function todo(state = { completed: false, deleted: false, updatedAt: undefined }, action) {
+export function todo(state = { completed: false, deleted: false }, action) {
   switch (action.type) {
     case ADD_TODO:
       return update(state, {
@@ -28,14 +28,12 @@ export default function todos(state = [], action) {
       })]]});
     case UPDATE_TODO:
       return update(state, { $splice: [[index, 1, update(todoItem, {
-        edit: { $set: false },
         text: { $set: action.text },
         updatedAt: { $set: Date.now() },
       })]]});
     case DELETE_TODO:
       return update(state, { $splice: [[index, 1, update(todoItem, {
         deleted: { $set: true },
-        edit: { $set: false },
       })]]});
     default:
       return state;
