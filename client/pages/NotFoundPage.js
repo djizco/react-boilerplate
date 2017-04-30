08d8499a78a1ba03d35a2cd160349212806553fc
Rico Rojas <djizco@yahoo.com>
2017-04-30 02:57:26 -0500

    Update styles

--- client/pages/NotFoundPage.js
+++ client/pages/NotFoundPage.js
@@ -4,7 +4,7 @@ export default function NotFoundPage() {
   return (
     <div className="lost-page">
       <div className="container">
-        <div className="section full-page">
+        <div className="section is-full-page">
           <h1 className="app-title title is-1">404</h1>
           <p>The page you requested does not exist.</p>
         </div>
