6ce18aa0e4cf6463fb840731d33896b2e39c3ceb
Rico Rojas <djizco@yahoo.com>
2017-05-28 18:35:14 -0500

    Fix import statement

--- client/pages/HomePage.js
+++ client/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
-import RaisedButton from 'material-ui/RaisedButton';
+import { RaisedButton } from 'material-ui';
 
 export default function HomePage() {
   const handleToCounter = () => browserHistory.push('counter');
