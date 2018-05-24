7d7e3fefd8b72f8e5c03d2bd586304cd42221a36
Rico Rojas <djizco@yahoo.com>
2018-05-24 16:31:04 -0500

    Add material Icons

--- client/components/sections/HomeSection/HomeSection.js
+++ client/components/sections/HomeSection/HomeSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 import { Button } from '@material-ui/core';
+import { ArrowForward } from '@material-ui/icons';
 
 export default function HomeSection() {
   const handleToCounter = () => browserHistory.push('counter');
@@ -12,9 +13,11 @@ export default function HomeSection() {
       <h1 className="title is-1">React App</h1>
       <Button variant="raised" color="primary" style={buttonStyles} onClick={handleToCounter}>
         Counter App
+        <ArrowForward />
       </Button>
       <Button variant="raised" color="primary" style={buttonStyles} onClick={handleToTodo}>
         Todo App
+        <ArrowForward />
       </Button>
     </div>
   );
