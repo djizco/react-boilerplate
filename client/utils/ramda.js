2db836a3379d09a86d8e0d639cd07204058df762
Rico Rojas <djizco@yahoo.com>
2018-10-31 11:22:07 -0500

    Fix helper function imports

--- client/utils/ramda.js
+++ client/utils/ramda.js
@@ -1,19 +1,27 @@
 import add       from 'ramda/src/add';
+import concat    from 'ramda/src/concat';
 import dec       from 'ramda/src/dec';
 import findIndex from 'ramda/src/findIndex';
+import head      from 'ramda/src/head';
 import inc       from 'ramda/src/inc';
 import map       from 'ramda/src/map';
 import pick      from 'ramda/src/pick';
 import propEq    from 'ramda/src/propEq';
 import reverse   from 'ramda/src/reverse';
+import tail      from 'ramda/src/tail';
+import toUpper   from 'ramda/src/toUpper';
 
 export default {
   add,
+  concat,
   dec,
   findIndex,
+  head,
   inc,
   map,
   pick,
   propEq,
   reverse,
+  tail,
+  toUpper,
 };
--- client/utils/strings.js
+++ client/utils/strings.js
@@ -1,3 +1,3 @@
-import R from 'ramda';
+import R from '_utils/ramda';
 
 export const capitalize = string => R.concat(R.toUpper(R.head(string)), R.tail(string));
